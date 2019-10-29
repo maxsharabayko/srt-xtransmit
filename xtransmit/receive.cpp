6c4603bf91f2ac0826304f41c45cfa48b13c0561
Maxim Sharabayko <maxlovic@gmail.com>
2019-10-29 16:10:39 +0100

    Added endline to receive::read_timestamp

--- xtransmit/receive.cpp
+++ xtransmit/receive.cpp
@@ -39,6 +39,7 @@ void read_timestamp(const vector<char>& buffer)
 
 	const auto delay = systime_now - (system_clock::from_time_t(send_time) + microseconds(send_time_us));
 	::cout << " delta " << duration_cast<milliseconds>(delay).count() << " ms";
+	::cout << endl;
 }
 
 
