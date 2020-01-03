06d8d74ec4d40728df188fdb015b8d22ea833803
Maxim Sharabayko <maxlovic@gmail.com>
2020-01-03 15:42:42 +0100

    [app] Fixed some CodeFactor warnings

--- xtransmit/srt_node.hpp
+++ xtransmit/srt_node.hpp
@@ -13,45 +13,32 @@ using namespace std;
 
 class SrtNode
 {
-
 public:
-
     SrtNode(const UriParser &src_uri);
-
     ~SrtNode();
 
-
 public:
-
     int Listen(int max_conn);
-
     int Connect();
-
     std::future<SRTSOCKET> AcceptConnection(const volatile std::atomic_bool& force_break);
-
     void Close() { m_stop_accept = true; }
 
 public:
-
     // Receive data
     // return     -2 unexpected error
     //            -1 SRT error
     //
     int Receive(char *buffer, size_t buffer_len, int *srt_socket_id);
 
-
     int Send(const char *buffer, size_t buffer_len, int srt_socket_id);
 
-
     int Send(const char *buffer, size_t buffer_len);
-
 
     int WaitUndelivered(int wait_ms);
 
     SRTSOCKET GetBindSocket() { return m_bindsock; }
 
 private:
-
     int EstablishConnection(bool caller, int max_conn);
 
     SRTSOCKET AcceptingThread(const volatile std::atomic_bool& force_break);
@@ -62,7 +49,6 @@ private:
     int ConfigureAcceptedSocket(SRTSOCKET sock);
 
 private:
-
     std::list<SRTSOCKET>      m_read_fifo;
     std::set<SRTSOCKET>       m_accepted_sockets;
 
@@ -74,7 +60,6 @@ private:
     std::promise<void>        m_accept_barrier;
 
 private:
-
     std::atomic<bool> m_stop_accept = { false };
     std::mutex        m_recv_mutex;
     std::mutex        m_accepted_sockets_mutex;
@@ -82,12 +67,9 @@ private:
     std::thread m_accepting_thread;
 
 private:    // Configuration
-
     std::string m_host;
     int m_port;
     std::map<string, string> m_options; // All other options, as provided in the URI
-
-
 };
 
 
