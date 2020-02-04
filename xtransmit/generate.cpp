1cb33cc8c35ca6d892289cb0f43d19dee7548edc
Maxim Sharabayko <maxlovic@gmail.com>
2020-02-04 17:27:10 +0100

    [generate] stats from 0

--- xtransmit/generate.cpp
+++ xtransmit/generate.cpp
@@ -60,14 +60,13 @@ void run_pipe(shared_sock dst, const config &cfg, const atomic_bool &force_break
 			return;
 		}
 
-		bool               print_header = true;
+		logfile_stats << sock->statistics_csv(true) << flush;
 		const milliseconds interval(cfg.stats_freq_ms);
 		while (!force_break && !local_break)
 		{
 			this_thread::sleep_for(interval);
 
-			logfile_stats << sock->statistics_csv(print_header) << flush;
-			print_header = false;
+			logfile_stats << sock->statistics_csv(false) << flush;
 		}
 	};
 
